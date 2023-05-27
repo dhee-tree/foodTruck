4403183aa6bce14366144f9fb3d048bace7de316
Ighomena Daniel Odebala <p110126653@student.peterborough.ac.uk>
2023-05-27 11:37:22 +0100

    SODA API integration

--- foodTruckApp/views.py
+++ foodTruckApp/views.py
@@ -1,5 +1,47 @@
+import requests
+from decouple import config
 from django.shortcuts import render
+
 
 # Create your views here.
 def index(request):
-    return render(request, 'index.html')
+    context = {
+        'trucks': get_contents(),
+        'number_range': range(20),
+    }
+    return render(request, 'index.html', context)
+
+
+def get_contents():
+    url = config('SODA_API')
+
+    # Send an HTTP GET request to the API endpoint
+    response = requests.get(url)
+
+    # Check if the request was successful (status code 200)
+    if response.status_code == 200:
+        # Get the JSON data from the response
+        data = response.json()
+
+        facility_dict = {}
+
+        # write a for loop to iterate over the length of the data
+        for i in range(len(data)):
+            # add data to facility dict use i as the key and a list containing the facility type, latitude, and longitude as values
+            facility_dict[i] = [data[i].get("facilitytype"), data[i].get("latitude"), data[i].get("longitude")]
+
+        # print(facility_dict)
+
+        # get the first 10 items from the facility dict
+        # facility_dict = dict(list(facility_dict.items())[0:10])
+
+        # get the truck with the key 10
+        print(facility_dict[10])
+        return facility_dict
+
+    else:
+        # If the request was not successful, print the status code
+        print(f"Request failed with status code: {response.status_code}")
+
+
+get_contents()
